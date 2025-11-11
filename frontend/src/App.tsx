aed71b674fa42f2a1ff7e84e3dcc9f0b25baaba5
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-11-11 18:27:47 +0100

    add s/l/d theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { HttpClient } from "./components/HttpClient";
 import {Box, Theme} from "@radix-ui/themes";
 import "./style.css";
+import {ThemeProvider} from "next-themes";
 
 function App() {
-    return (
-      <Theme accentColor="tomato" radius="small" style={{ width: '100vw' }}>
-        <Box p="2">
-          <HttpClient />
-        </Box>
-      </Theme>
+  return (
+      <ThemeProvider attribute="class">
+        <Theme accentColor="tomato" radius="small" style={{ width: '100vw' }}>
+          <Box p="2">
+            <HttpClient />
+          </Box>
+        </Theme>
+      </ThemeProvider>
     )
 }
 
